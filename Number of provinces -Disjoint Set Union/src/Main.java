5188686cf72a95ed857599b3fd4ea0ed07d7e08f
1 <1@ROG>
2025-03-08 17:42:50 +0800

    1

--- Number of provinces -Disjoint Set Union/src/Main.java
+++ Number of provinces -Disjoint Set Union/src/Main.java
@@ -19,6 +19,7 @@ public class Main {
         for (int i = 0; i < cities; i++) {
             head[i] = i;
             level[i] = 1; // 初始化为1
+
         }
 
         for (int i = 0; i < cities; i++) {
