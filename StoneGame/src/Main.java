36d7033f0a24c0a1bcf37998bc2f2c2dc4528273
1 <1@ROG>
2025-03-09 17:35:13 +0800

    3.9区间最小值递归返回，动态规划

--- StoneGame/src/Main.java
+++ StoneGame/src/Main.java
@@ -15,8 +15,35 @@ public class Main {
         int p1 = maxScore(arr, 0, arr.length - 1);
         // 判断并打印玩家一的最大得分是否大于另一个玩家的得分
         System.out.println(p1 > sum - p1);
+        System.out.println(maxScore1(arr, 0, arr.length - 1));
+        System.out.println(dp(arr));
     }
 
+    /**
+     * 动态规划方法，用于计算在给定数组中，玩家一是否能赢得玩家二
+     * 通过构建一个二维数组dp来存储玩家一相对于玩家二的得分差的最大值
+     * @param arr 给定的整数数组，代表每个元素的得分
+     * @return 如果玩家一能赢得玩家二，则返回true；否则返回false
+     */
+    static boolean dp(int[] arr){
+        // 数组的长度
+        int length=arr.length;
+        // 初始化一个二维数组用于动态规划
+        int [][]dp=new int[length][length];
+        // 初始化对角线元素，即只有一个元素时的情况时候，差值即为该元素
+        for (int i = 0; i <length ; i++) {
+            dp[i][i]=arr[i] ;
+        }
+        // 从右下角开始填充dp数组，计算所有可能的得分差的最大值
+        for (int i = length-1; i >=0 ; i--) {
+            for (int j = i+1; j <length ; j++) {
+                // 玩家一可以选择左边或右边的元素，选择使其与玩家二的得分差最大的策略
+                dp[i][j]=Math.max(arr[i]-dp[i+1][j],arr[j]-dp[i][j-1]);
+            }
+        }
+        // 如果玩家一的得分差最大值大于0，则玩家一可以赢得玩家二
+        return  dp[0][length-1]>0;
+    }
     /**
      * 计算给定数组中，玩家一从索引l到r的最大得分
      * @param arr 代表游戏得分的整数数组
@@ -50,8 +77,22 @@ public class Main {
         // 返回左侧和右侧得分中的最大值
         return Math.max(sright, sleft);
     }
+    static int maxScore1(int[] arr, int l, int r){
+        // 当只有一个元素时，返回该元素的值
+        if (l == r) {
+            return arr[l];
+        }
 
+        int  sright = arr[r] -maxScore(arr, l, r  -1);
+
+
+
+        int sleft = arr[l] -maxScore(arr, l +1, r);
+     return  Math.max(sright, sleft);
     }
+
+
+}
 
 
 
