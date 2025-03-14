b4288cd92fd28479e47e510d224c919b21074086
1 <1@ROG>
2025-03-14 16:07:56 +0800

    3.14 下标不连续地找出最大和

---
+++ Rob/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println(maxMoney(new int[]{1,2,3,1},3));
+        System.out.println(maxMoney1(new int[]{1,2,3,1},4));
+    }
+
+      static int maxMoney(int [] nums,int index){
+        //递归
+        if (nums==null||index<0)
+        {
+            return 0;
+        }
+        if (index == 0){
+                return nums[0];
+            }
+
+        return Math.max(maxMoney(nums,index-2)+nums[index],maxMoney(nums,index-1));
+
+    }
+    static int maxMoney1(int [] nums,int length){
+        // 动态规划
+        if (nums==null)
+        {
+            return 0;
+        }
+        if (length == 1){
+            return nums[0];
+        }
+   int[] dp=new int[length];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+
+        for (int i = 2; i <length ; i++) {
+            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+            return  dp[length-1];
+    }
+}
