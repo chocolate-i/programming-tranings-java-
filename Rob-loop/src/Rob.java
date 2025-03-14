0922e3da0244db7b18a45730d7a6aff5f6b760e3
1 <1@ROG>
2025-03-14 16:45:12 +0800

    3.14 首尾相连的情况 不连续地找出最大和

--- Rob-loop/src/Rob.java
+++ Rob-loop/src/Rob.java
@@ -1,26 +1,29 @@
 public class Rob {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println(maxMoney(new int[]{1,2,3,1},4));
+        int[] nums=new int[]{1,2,3,1};
+        //有环的情况下，第一个或者最后一个只能选其一
+        System.out.println(maxMoney(nums,0,nums.length-2));
+        System.out.println(maxMoney(nums,1,nums.length-1));
     }
 
-    static int maxMoney(int [] nums,int length){
+    static int maxMoney(int [] nums,int start,int end){
         // 动态规划
         if (nums==null)
         {
             return 0;
         }
-        if (length == 1){
-            return nums[0];
+         int first=nums[start];
+        int second=Math.max(nums[start],nums[start+1]);
+        for (int i = start+2; i <=end ; i++) {
+            int temp=Math.max(first+nums[i],second);
+            first=second;
+            second=temp;
         }
-        int[] dp=new int[length];
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
-
-        for (int i = 2; i <length ; i++) {
-            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
-        }
-        return  dp[length-1];
+        return  second;
     }
+
+
+
 
 }
