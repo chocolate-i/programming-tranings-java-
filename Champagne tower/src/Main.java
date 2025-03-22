b97084c39f68850ca84a653101e8bb4d58764dd7
1 <1@ROG>
2025-03-22 17:23:04 +0800

    3.12 香槟塔

--- Champagne tower/src/Main.java
+++ Champagne tower/src/Main.java
@@ -6,21 +6,34 @@ public class Main {
         System.out.println(Champagnetower(5,2,1));
     }
 
-    public  static double Champagnetower(int poured,int query_row,int query_glass){
-        double[][]ca=new double [100][100];
-        ca[0][0]=poured;
-        for (int i = 0; i <=query_row; i++) {
+    /**
+     * 计算香槟塔中特定杯子的香槟量
+     *
+     * @param poured 倒入顶部杯子的香槟量
+     * @param query_row 查询的行号（从0开始）
+     * @param query_glass 查询的杯子号（在指定行内，从0开始）
+     * @return 指定杯子中的香槟量，返回值不会超过1，因为杯子的容量是有限的
+     */
+    public static double Champagnetower(int poured, int query_row, int query_glass) {
+        // 初始化一个二维数组来存储每个杯子的香槟量
+        double[][] ca = new double[100][100];
+        // 将所有的香槟倒入顶部杯子
+        ca[0][0] = poured;
+        // 遍历每一行的杯子，直到查询的行
+        for (int i = 0; i <= query_row; i++) {
             for (int j = 0; j <= i; j++) {
-            double d=(ca[i][j]-1.0)/2;
-            if (d>0){
-                ca[i+1][j]+=d;
-                ca[i+1][j+1]+=d;
-            }
-
+                // 计算当前杯子溢出的香槟量，如果有的话，会均分到下一行的两个杯子中
+                double d = (ca[i][j] - 1.0) / 2;
+                if (d > 0) {
+                    // 香槟溢出，分配到下一行的两个杯子中
+                    ca[i + 1][j] += d;
+                    ca[i + 1][j + 1] += d;
+                }
             }
         }
-        return Math.min(1,ca[query_row][query_glass]);
-
+        // 返回指定杯子中的香槟量，确保返回值不超过1
+        return Math.min(1, ca[query_row][query_glass]);
     }
+
 
 }
